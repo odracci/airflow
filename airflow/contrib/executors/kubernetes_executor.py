0e0bbd3cdba18b9f39ae9dcd7cc42803a416b7ef
Riccardo Bini <odracci@gmail.com>
2018-12-05 11:19:28 +0100

    [AIRFLOW-3319] - KubernetsExecutor: Need in try_number in labels if getting them later

--- airflow/contrib/executors/kubernetes_executor.py
+++ airflow/contrib/executors/kubernetes_executor.py
@@ -333,6 +333,7 @@ class AirflowKubernetesScheduler(LoggingMixin):
             namespace=self.namespace, worker_uuid=self.worker_uuid,
             pod_id=self._create_pod_id(dag_id, task_id),
             dag_id=dag_id, task_id=task_id,
+            try_number=try_number,
             execution_date=self._datetime_to_label_safe_datestring(execution_date),
             airflow_command=command, kube_executor_config=kube_executor_config
         )
@@ -445,10 +446,16 @@ class AirflowKubernetesScheduler(LoggingMixin):
 
     def _labels_to_key(self, labels):
         try:
+            try_num = 1
+            try:
+                try_num = int(labels.get('try_number', '1'))
+            except ValueError:
+                self.log.warn("could not get try_number as an int: %s", labels.get('try_number', '1'))
             return (
                 labels['dag_id'], labels['task_id'],
                 self._label_safe_datestring_to_datetime(labels['execution_date']),
-                labels['try_number'])
+                try_num
+            )
         except Exception as e:
             self.log.warn(
                 'Error while converting labels to key; labels: %s; exception: %s',
